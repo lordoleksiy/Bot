88ca0983c26b014984729b71b44be5aee56b6cd2
YaroslavBugayov <bugayov576@gmail.com>
2022-03-01 00:17:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '5290656003:AAHs-MnL_wUOwDh18i-xgfxUx-JdPxSZ30c';
-const bot = new TelegramBot(token, {polling: true});
+const { Telegraf } = require('telegraf')
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привіт, молоді геї!');
-  });
+const bot = new Telegraf("5290656003:AAHs-MnL_wUOwDh18i-xgfxUx-JdPxSZ30c")
+
+bot.start(ctx=>{ctx.reply("Ярык хуй моржовый")})
+
+bot.on('message', (message)=> {
+  console.log(message)
+  bot.sendMessage(message.chat.id, "Well Cum!")
+});
+
+bot.launch()
