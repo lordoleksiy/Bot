9b8990cbc6ed6377629122835a229151165011c5
lordoleksiy <lordoleksiy@gmail.com>
2022-02-28 23:07:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5290656003:AAHs-MnL_wUOwDh18i-xgfxUx-JdPxSZ30c';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет, Друг!');
+  });
